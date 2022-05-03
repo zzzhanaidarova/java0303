dbc96070efe2697960c08f26b675da92ea8722de
zzzhanaidarova <212285@astanait.edu.kz>
2022-05-03 14:20:37 +0600

    Initial commit

---
+++ src/com/company/BST.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class BST {
+    class MyNode{
+        MyNode left, right;
+
+        public MyNode(int item){
+            left = right = null;
+        }
+    }
+}
