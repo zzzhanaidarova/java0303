01d7a4dcfac883bbb5e7db0b2af2e2d5b54e4f21
zzzhanaidarova <212285@astanait.edu.kz>
2022-05-04 20:53:09 +0600

    Creating size,get, and put methods in BST

--- src/com/company/BST.java
+++ src/com/company/BST.java
@@ -1,11 +1,58 @@
 package com.company;
 
-public class BST {
-    class MyNode{
-        MyNode left, right;
+import sun.awt.SunHints;
+import java.security.Key;
+import java.util.prefs.NodeChangeEvent;
 
-        public MyNode(int item){
+public class BST<Key extends Comparable<Key>, Value>{
+    private MyNode root;
+    private class MyNode{
+        private MyNode left, right;
+        private int n;
+        private Key key;
+        private Value val;
+
+        public MyNode(Key key, Value val, int n){
+            this.key = key;
+            this.val = val;
+            this.n = n;
             left = right = null;
         }
     }
+
+    public int size(){
+        return size(root);
+    }
+
+    private int size(MyNode root){
+        if (root == null) return 0;
+        else return root.n;
+    }
+
+    public Value get(Key key){
+        return get(root, key);
+    }
+
+    private Value get(MyNode root, Key key){
+        if (root == null) return null;
+        int compare = key.compareTo(root.key);
+        if (compare < 0) return get(root.left, key);
+        else if (compare > 0) return get(root.right, key);
+        else return root.val;
+    }
+
+    public void put(Key key, Value val){
+        root = put(root, key, val);
+    }
+
+    private MyNode put(MyNode root, Key key, Value val){
+        if (root == null) return new MyNode(key, val, 1);
+        int compare = key.compareTo(root.key);
+        if (compare < 0) root.left = put(root.left, key, val);
+        else if (compare > 0) root.right = put(root.right, key, val);
+        else root.val = val;
+        root.n = size(root.left) + size(root.right) + 1;
+        return root;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+		BST tree = new BST();
+		System.out.println("Binary Search Tree");
+		int j=10;
+		for (int i=0; i<10; i++){
+			tree.put(i, j);
+			j+=10;
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.println(tree.get(i));
+		}
+		System.out.println("Size: " + tree.size());
+	}
 }
