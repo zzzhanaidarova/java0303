6fe053efce259cef62c138ecf26a9d669ae46d23
zzzhanaidarova <212285@astanait.edu.kz>
2022-05-07 22:31:50 +0600

    Creating HashNode class and its get and put methods

---
+++ src/com/company/HashNode.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import javax.xml.soap.Node;
+
+public class HashNode<Key, Value> {
+    private HashNode head;
+    private Key key;
+    private Value value;
+    private HashNode next;
+    public HashNode(Key key, Value value, HashNode next){
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public Value get(Key key){
+        for (HashNode root = head; root != null; root = root.next)
+            if(key.equals(root.key)) return (Value) root.value;
+        return null;
+    }
+
+    public void put(Key key, Value value){
+        for (HashNode root = head; root != null; root = root.next)
+            if (key.equals(root.key)){
+                root.value = value;
+                return;
+            }
+        head = new HashNode(key, value, head);
+    }
+
+    @Override
+    public String toString(){
+        return "{" + key + " " + value + "}";
+    }
+}
