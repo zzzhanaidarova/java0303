4f1dda71862efcdafa71a2b217b488452b1f552d
zzzhanaidarova <212285@astanait.edu.kz>
2022-05-07 22:29:59 +0600

    Creating HashNode class and its get and put methods

--- src/com/company/MyHashTable.java
+++ src/com/company/MyHashTable.java
@@ -1,4 +1,11 @@
 package com.company;
 
-public class MyHashTable {
+public class MyHashTable<Key, Value> {
+    private int M;
+    private int size;
+    private HashNode<Key, Value>[] chainArray;
+
+    private int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % size;
+    }
 }
